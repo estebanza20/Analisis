1f05d38a5afbda08c3bfa5af882b4615e48323f1
daniel garcia vaglio <degv364@gmail.com>
2016-04-08 09:02:21 -0600

    Anadir graficas al reporte, ejercicios 3 y 6

--- matlab_01/Ejercicio6.m
+++ matlab_01/Ejercicio6.m
@@ -7,7 +7,7 @@ soluciones=[];
 
 %Se itera sobre uan seccion del dominio para encontrar un valor donde la 
 %funcion cambie de signo
-plot (x,y);
+
 for i=2:tamano(2)
     if y(i)*y(i-1)<0
         %en medio de estos dos hay un cambio de signo, portanto hay una
@@ -16,6 +16,16 @@ for i=2:tamano(2)
         soluciones=[soluciones solucionParticular(x(i-1), x(i), y(i-1), y(i))];
     end
 end
+%Se imprimen las soluciones
 soluciones
-
+%se grafica la funcion
+subplot(2,1,1), plot (x,y);
+title('Grafica de la funcion')
+%Se grafica una porcion mas pequena para ver las soluciones
+cantSoluciones=size(soluciones);
+x=soluciones(1)-1:0.01:soluciones(cantSoluciones(2))+1;
+y=x.^3+x.^2+6*x+55*cos(x)-10*x.*exp(0.2*x)-(x.^2).*exp(-0.3*x);
+cero=0*x;
+subplot(2,1,2), plot (x,y, x, cero)
+title('Soluciones')
 
