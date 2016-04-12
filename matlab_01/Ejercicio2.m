1cc8cc25bebd445e9a22315b99e9ec3a82cf9944
Jose Ariel <jafp0302@gmail.com>
2016-04-12 08:22:50 -0600

    Ejercicio2

---
+++ matlab_01/Ejercicio2.m
@@ -0,0 +1,38 @@
+%Punto a
+A=randi(10,10,10)
+
+%Punto b
+VecProp= eig(A, 'matrix');
+valoProp=eig(A);
+
+% Punto c
+B=randi(10,10,3);
+
+%Punto d
+C = A*B
+Size_C = size(C);
+Max_C = max(C(:));
+Min_C = min(C(:));
+
+%Punto e
+
+x = A(5,2)*B(9,3)*C(5,3);
+
+%Punto f
+
+concavec = cat(2,A(3,:),B(6,:))
+
+%Punto g
+
+Goku = diag(diag(A))
+Vegueta = inv(Goku)
+
+%Punto h
+
+M = magic(20)
+
+sudoku = sum(M)
+
+wow = sum(diag(M))
+
+plot(M)
